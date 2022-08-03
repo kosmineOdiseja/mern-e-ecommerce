8faf0aa5e6792abec6200821f447d3f2150ace2b
Alius <aliusbosas@gmail.com>
2022-08-03 20:47:03 +0300

    data file fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import prodcuts from './data/products.js'
+import products from './data/products.js'
 dotenv.config()
 
 const app = express()
