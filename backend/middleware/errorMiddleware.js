f81ff04d336325674c94e17be48944c96ae0c7da
Alius <aliusbosas@gmail.com>
2022-08-08 14:40:46 +0300

    putting all into milddleware folder

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,22 @@
+
+const notFound = (req, res, next) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`)
+	res.status(404)
+	next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+
+	// sometimes we get a 200 status code,  even if it's an error. 
+	// so this line puts things in the right boxes.
+
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+	res.status(statusCode)
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+
+	})
+}
+
+export default { notFound, errorHandler }
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js'
 import colors from 'colors'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 
@@ -17,24 +18,9 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 
 // middleware  error
-app.use((req, res, next) => {
-	const error = new Error(`Not Found - ${req.originalUrl}`)
-	res.status(404)
-	next(error)
-})
+app.use(notFound)
 
-app.use((err, req, res, next) => {
-
-	// sometimes we get a 200 status code,  even if it's an error. 
-	// so this line puts things in the right boxes.
-	const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-	res.status(statusCode)
-	res.json({
-		message: err.message,
-		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-
-	})
-})
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
